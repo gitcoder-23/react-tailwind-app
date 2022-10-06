e6c41188c2a8817c641ca7a87128b2d32005ea25
prabhat-cn <prabhat@capitalnumbers.com>
2022-10-06 16:19:19 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Getting Started with Create React Tailwind App
+
+npx create-react-app my-project
+cd my-project
+
+yarn add -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p
+
+## For Icons
+
+https://react-icons.github.io/react-icons
+
+### For typing
+
+yarn add react-typed
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Analytics from './components/Analytics';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Analytics />
+    </div>
+  );
+}
+
+export default App;
